d85a377515599b37a1d22be08786d0891a293aa0
sijibomi <gbemilesanmi@gmail.com>
2023-12-30 05:28:20 +0100

    last og fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,10 @@ import { inter } from '@/lib/fonts'
 
 export const metadata: Metadata = {
     title: 'oluwasijibomi',
-    description: 'i live so i love.',
+    description: 'my fucking website.',
     openGraph:{
-        title:"siji's website",
-        description:"i live so that i can love",
+        title:"oluwasijibomi",
+        description:"guys it's my website, check it out.",
         images:"https://opengraph.b-cdn.net/production/documents/6f6fe320-ad5f-4c32-ae18-8bd71717f445.jpg?token=_Uqx-HbKtoYMzIEDNJu12usQP2ygfPpkCh_U1LjyHvk&height=675&width=1200&expires=33239909303"
     }
 }
