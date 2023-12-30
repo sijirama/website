27d120fa5f2d87550840f4b14072349ba36f071a
sijibomi <gbemilesanmi@gmail.com>
2023-12-30 16:47:16 +0100

    blog almsot done

---
+++ src/components/blog/PostCard.tsx
@@ -0,0 +1,27 @@
+import { MyDate } from "@/lib/date";
+import { manrope } from "@/lib/fonts";
+import { PostMetaData } from "@/lib/getPostMetaData";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  post: PostMetaData;
+}
+
+function PostCard({ post }: Props) {
+  return (
+    <Link href={`writing/${post.slug}`} className="w-full">
+      <div
+        className={`p-2 rounded-md my-2 hover:bg-zinc-100 transition-colors duration-300 ${manrope.className} `}
+      >
+        <h1 className="mb-1 font-semibold underline underline-offset-2">
+          {post.title}
+        </h1>
+        <p className="tracking-tight font-light">{post.description}</p>
+        <p className="text-xs font-light">{MyDate(post.date)}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
---
+++ src/lib/getPostMetaData.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+import matter from "gray-matter";
+
+export interface PostMetaData {
+  title: string;
+  date: string;
+  description: string;
+  slug: string;
+  thumbnail: string;
+  readTime: string;
+}
+
+const format = ".mdx";
+
+export function getPostMetaData(): PostMetaData[] {
+  const folder = "src/posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(format));
+
+  const posts = markdownPosts.map((filename) => {
+    const fileContents = fs.readFileSync(`src/posts/${filename}`, "utf8");
+    const matterResult = matter(fileContents);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      description: matterResult.data.description,
+      slug: filename.replace(format, ""),
+      thumbnail: matterResult.data.thumbnail,
+    };
+  });
+
+  return posts;
+}
+
+export function getPostContent(slug: string) {
+  const folder = "src/posts/";
+  const file = `${folder}/${slug}${format}`;
+  const content = fs.readFileSync(file, "utf-8");
+  const matterResult = matter(content);
+  return matterResult;
+}
