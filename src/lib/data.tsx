d3c7a290309eaae7f0d6517c9c0cc4999abef43c
sijirama <gbemilesanmi@gmail.com>
2024-02-22 00:21:50 +0100

    done again

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -54,12 +54,14 @@ export const projects = [
         "title": "Bauble",
         "description": "Streamlining university project management for student groups and ensures an efficient project experience.",
         "type": "Final Year Project",
+        "link":"https://baublespace.vercel.app/",
         "technologies": ["TypeScript", "Next.js", "TailwindCSS", "PostgreSQL", "Prisma"]
     },
     {
         "title": "Monko (WIP)",
-        "description": "simple interpreter built in Golang",
+        "description": "simple experimental interpreter built in Golang, mostly for research on lexical analysis and token parsing",
         "type": "Side Project",
+        "link":"",
         "technologies": ["Golang"]
     },
     {
@@ -72,6 +74,7 @@ export const projects = [
         "title": "Accord",
         "description": "fullstack messaging application inspired by Discord, channel management, real-time messaging etc.",
         "type": "Final Year Project",
+        "link":"https://accordingly.vercel.app/",
         "technologies": ["TypeScript", "Next.js", "TailwindCSS", "PostgreSQL", "Prisma"]
     },
 
@@ -85,6 +88,7 @@ export const projects = [
         "title": "Tidy",
         "description": "CLI application for organization and task tracking",
         "type": "Side Project",
+        "link":"https://github.com/sijirama/tidy",
         "technologies": ["GoLang", "SQlite", "BubbleTea"]
     },
     {
@@ -98,7 +102,16 @@ export const projects = [
         "title": "Chip*",
         "description": "Simple chip8 emulator built with javascript.",
         "type": "Side Project",
+        "link":"https://sijirama.github.io/chip8/",
         "technologies": ["JavaScript"]
     },
+    {
+        "title": "Particles",
+        "description": "research repository for particle life cycles under various conditions.",
+        "type": "Side Project",
+        "link":"https://sijirama.github.io/particles/",
+        "technologies": ["JavaScript"]
+    },
+
 
 ]
