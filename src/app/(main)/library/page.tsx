c7a547d0f9b37847a770d6035315e84c2b84b83e
sijirama <gbemilesanmi@gmail.com>
2024-04-09 18:10:26 +0100

    garden explorer

--- src/app/(main)/library/page.tsx
+++ src/app/(main)/library/page.tsx
@@ -1,8 +1,28 @@
 "use client"
-import { GetItemForPath, isTherePathStored } from '@/store/pathStorage'
 import { useRouter } from 'next/navigation'
-import React from 'react'
 export default function Page() {
+    const label = "ExplorerPath"
+
+    function SetItemForPath(path: string) {
+        if (typeof window !== undefined) {
+            localStorage.setItem(label, path)
+        }
+    }
+
+    function GetItemForPath() {
+        if (typeof window !== undefined) {
+            return localStorage.getItem(label)
+        }
+    }
+
+    function isTherePathStored() {
+        let pathStored
+        if (typeof window !== undefined) {
+            pathStored = localStorage.getItem(label)
+        }
+        return pathStored ? true : false
+    }
+
 
     const router = useRouter()
 
