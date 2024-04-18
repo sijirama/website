7f8af3a19da6dc832563ac931ed58424ae47eec2
sijirama <gbemilesanmi@gmail.com>
2024-04-18 20:04:10 +0100

    .

--- src/components/PageAbout.tsx
+++ src/components/PageAbout.tsx
@@ -26,21 +26,21 @@ export default function JobHunt() {
     return <section className={`w-full h-full text-zinc-900 dark:text-zinc-300 items-center flex ${manrope.className} text-sm lg:text-base `}>
         <div className="w-full lg:w-[95%] flex gap-3 md:gap-1.5 flex-col items-left h-full md:py-2">
             <p id='target'>
-                hi, my name is <SpanWord word=" Oluwasijibomi Ilesanmi " />, a software developer based in Lagos, Nigeria. I have a deep passion for crafting applications that not only solve problems but also contribute positively to the well-being of humanity. I <SpanWord word="love music" link="https://open.spotify.com/user/31okn5a72nq67rs2sndgdoudfm3y" /> and listen mostly to R&B, Rap and occasionally Afro beats. i also love reading books, both fictional and non-fictional, you probrably should check out the notes and articles i have written <SpanWord word="Here in my Digital Garden" link="/library" />, i am also in search of a job, and my <SpanWord word="Portfolio can be found here." link="/portfolio" /></p>
+                my name is <SpanWord word=" Oluwasijibomi Ilesanmi " />, a software developer based in Lagos, Nigeria. I have a deep passion for crafting applications that not only solve problems but also contribute positively to the well-being of people around me. I <SpanWord word="love music" link="https://open.spotify.com/user/31okn5a72nq67rs2sndgdoudfm3y" /> and listen mostly to R&B, Rap and occasionally Afro beats. i also love reading books, both fictional and non-fictional, you probrably should check out the notes and articles i have written <SpanWord word="Here in my library" link="/library" />, if you want more info on my professional background, the details can be found here  <SpanWord word="in my portfolio page" link="/portfolio" /></p>
             <div>
                 <p>i am really really really excited about ...</p>
                 <ul className="pl-4 list-disc">
                     <li>
-                        the interesting discourse at intersection of science and religion,
+                        the interesting discourse at the intersection of science and religion,
                         exploring nuanced discussions on origins, evolution, and ultimate
-                        implicaions for humanity.
+                        implicaions for humanity, i grew up in a christian home, but have the mind of a scientist, things are bound to clash.
                     </li>
-                    <li>engineering endeavors that have the potential to positively transform lives, whether through innovative technologies or sustainable solutions.</li>
+                    <li>engineering endeavors that have the potential to positively transform lives, i believe that this talent i have been given by God, i should try and use it to help people too.</li>
                     <li>
                         music and people that share music, sharing music is a love
-                        language.
+                        language, beautiful music is the art of the prophets that can calm the agitations of the soul; it is one of the most magnificent and delightful presents God has given us.
                     </li>
-                    <li>the future and what it holds for us.</li>
+                    <li>the future and what it holds for us, are we really all gonna make it?</li>
                 </ul>
             </div>
             <p>
--- src/components/miniportfolio.tsx
+++ src/components/miniportfolio.tsx
@@ -5,15 +5,15 @@ import { manrope } from "@/lib/fonts";
 export function MiniPortfolio() {
 
     return (
-        <section className={`flex-1 ${manrope.className} text-sm md:text-base md:w-[95%] mx-auto flex flex-col gap-5 my-3 py-6 border-y-zinc-900 dark:border-y-zinc-500 border-y text-zinc-900 dark:text-zinc-300 `}>
+        <section className={`flex-1 ${manrope.className} text-sm lg:text-base md:w-[95%] mx-auto flex flex-col gap-5 my-3 py-6 border-y-zinc-400 dark:border-y-zinc-700 border-y-[0.1px] text-zinc-900 dark:text-zinc-300 `}>
             <div className="grid md:grid-cols-4 md:grid-row-3 h-full w-full">
                 <div className="md:col-span-1 row-span-3 py-4 ">
                     <p >Experience</p>
                 </div>
                 <div className="md:col-span-3 md:row-span-3">
                     {expereince.map((exp, index) => (
-                        <div key={index} className=" gap-2 items-center justify-between border-b dark:border-zinc-600 border-zinc-900 w-full h-auto py-4 grid grid-cols-3 transition-colors duration-300 hover:text-orange-800 ">
-                            <p className="col-span-1 font-semibold ">{exp.role}</p>
+                        <div key={index} className=" gap-2 items-center justify-between border-b dark:border-zinc-700 border-zinc-400 w-full h-auto py-4 grid grid-cols-3 transition-colors duration-300 hover:text-orange-800 ">
+                            <p className="col-span-1">{exp.role}</p>
                             <p className="col-span-1">{exp.name}</p>
                             <p className="col-span-1">{exp.period}</p>
                         </div>
@@ -26,7 +26,7 @@ export function MiniPortfolio() {
                 </div>
                 <div className="md:col-span-3 md:row-span-3">
                     {education.map((exp, index) => (
-                        <div key={index} className="items-center justify-between pb-5 dark:border-zinc-600 border-zinc-900 w-full h-auto py-4 grid grid-cols-3 hover:text-orange-800">
+                        <div key={index} className="items-center justify-between pb-5 dark:border-zinc-700 border-zinc-400 w-full h-auto py-4 grid grid-cols-3 hover:text-orange-800">
                             <p className="col-span-1">{exp.name}</p>
                             <p className="col-span-1">{exp.description}</p>
                             <p className="col-span-1">{exp.period}</p>
