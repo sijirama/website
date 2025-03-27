8d11ad8649233117d834c074047821fdb19468f8
sijirama <gbemilesanmi@gmail.com>
2025-03-27 23:58:09 +0100

    semaphores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="h-full overflow-x-hidden flex flex-col">
       <Header />
-      <div className="px-1 w-full md:max-w-4xl xl:max-w-3xl mx-auto">
+      <div className="px-1 w-full mt-3 md:mt-4 md:max-w-4xl xl:max-w-3xl mx-auto">
         <LandingPage />
       </div>
     </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,11 @@
 "use client";
-import Link from "next/link";
 import { ThemeToggle } from "@/providers/theme-toggle";
-//import { FaRegCircle } from "react-icons/fa";
 
 export default function Header({}) {
   return (
     <header className="z-10 w-full ">
       <main className="z-50 flex justify-between w-[95%] mx-auto py-5 rounded-lg text-zinc-900 dark:text-zinc-300">
-        <div className="hover:text-orange-800">
-          <Link href="/" className="-tracking-widest font-bold">
-            home
-          </Link>
-        </div>
+        <div className="hover:text-orange-800"></div>
         <div className="flex items-center gap-2">
           <div className="hover:text-orange-800 hidden">
             <ThemeToggle />
