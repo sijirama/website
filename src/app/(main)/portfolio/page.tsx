df6518e132963533e1c8939f548b5be581eab084
sijirama <gbemilesanmi@gmail.com>
2025-03-29 20:11:55 +0100

    main

--- src/app/(main)/portfolio/page.tsx
+++ src/app/(main)/portfolio/page.tsx
@@ -1,133 +1,182 @@
 import lagos from "../../../../public/images/pfp.jpg";
 import { expereince, education, skills, projects } from "@/lib/data";
 import { SlGlobe } from "react-icons/sl";
-import { IoLogoGithub, IoLogoLinkedin, IoLogoTwitter, IoMailOutline } from "react-icons/io5";
+import {
+  IoLogoGithub,
+  IoLogoLinkedin,
+  IoLogoTwitter,
+  IoMailOutline,
+} from "react-icons/io5";
+import { FaRegFile } from "react-icons/fa";
 import { manrope } from "@/lib/fonts";
 
 export default function Page() {
+  const iconStyle = "text-base md:text-lg dark:text-zinc-300 text-zinc-500 ";
 
-    const iconStyle = "text-base md:text-lg dark:text-zinc-300 text-zinc-500 "
+  const socials = [
+    {
+      icon: <IoMailOutline className={`${iconStyle}`} />,
+      url: "mailto:gbemilesanmi@gmail.com",
+    },
+    {
+      icon: <IoLogoGithub className={`${iconStyle}`} />,
+      url: "https://github.com/sijirama",
+    },
+    {
+      icon: <IoLogoLinkedin className={`${iconStyle}`} />,
+      url: "https://www.linkedin.com/in/oluwasijibomi-ilesanmi-8504b123a/",
+    },
+    {
+      icon: <IoLogoTwitter className={`${iconStyle}`} />,
+      url: "https://twitter.com/sijiramakun",
+    },
+    {
+      icon: <FaRegFile className={`${iconStyle}`} />,
+      url: "https://flowcv.com/resume/qjwdae4u1j",
+    },
+  ];
 
-    const socials = [
-        {
-            "icon": <IoMailOutline className={`${iconStyle}`} />,
-            "url": "mailto:gbemilesanmi@gmail.com"
-        },
-        {
-            "icon": <IoLogoGithub className={`${iconStyle}`} />,
-            "url": "https://github.com/sijirama"
-        },
-        {
-            "icon": <IoLogoLinkedin className={`${iconStyle}`} />,
-            "url": "https://www.linkedin.com/in/oluwasijibomi-ilesanmi-8504b123a/"
-        },
-        {
-            "icon": <IoLogoTwitter className={`${iconStyle}`} />,
-            "url": "https://twitter.com/sijiramakun"
-        }
-    ]
-
-
-    return (
-        <main className={` ${manrope.className} flex min-h-screen flex-col gap-4 items-center mx-auto pt-2 w-full md:4/5 lg:w-4/6 xl:w-3/6 text-zinc-950  dark:text-zinc-100 -tracking-widest p-2.5  md:p-5`}>
-            <div className="flex justify-between items-center w-full">
-                <div className="w-full md:w-3/5 flex flex-col gap-2">
-                    <p className="font-extrabold -tracking-wide text-lg md:text-xl">
-                        Ilesanmi Oluwasijibomi Gbemileke.
+  return (
+    <main
+      className={` ${manrope.className} flex min-h-screen flex-col gap-4 items-center mx-auto pt-2 w-full md:4/5 lg:w-4/6 xl:w-3/6 text-zinc-950  dark:text-zinc-100 -tracking-widest p-2.5  md:p-5`}
+    >
+      <div className="flex justify-between items-center w-full">
+        <div className="w-full md:w-3/5 flex flex-col gap-2">
+          <p className="font-extrabold -tracking-wide text-lg md:text-xl">
+            Ilesanmi Oluwasijibomi Gbemileke.
+          </p>
+          <p className="text-xs md:text-sm tracking-wide">
+            Full Stack Engineer focused on building products with extra
+            attention to details
+          </p>
+          <p className="font-light text-xs tracking-wide flex gap-1 items-center justify-start">
+            <SlGlobe className="" />
+            Victoria Island, Lagos, NG.
+          </p>
+          <div className="w-full flex gap-2 flex-wrap py-1 md:py-3">
+            {socials.map((social, i) => (
+              <a key={i} href={`${social.url}`} target="_blank">
+                <div className=" p-2 md:p-3 rounded-lg border border-zinc-500 dark:hover:bg-zinc-800 hover:bg-zinc-300 transition-colors duration-300">
+                  {social.icon}
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
+        {/**/}
+        {/*
+         */}
+        <div
+          style={{
+            backgroundImage: `url(${lagos.src})`,
+          }}
+          className="w-1/5 hidden md:block rounded-lg bg-cover bg-center bg-no-repeat h-36 p-2 bg-red-800"
+        ></div>
+      </div>
+      <div className="text-start  w-full flex flex-col gap-2">
+        <p className="font-extrabold -tracking-wide text-lg">About</p>
+        <p className="text-xs md:text-base tracking-wide text-zinc-900 dark:text-zinc-100">
+          As a dedicated Full Stack Engineer, my passion lies in building
+          technology that positively impacts and aids people. I am consistently
+          excited about the opportunity to create, innovate, and contribute to
+          meaningful solutions. Whether it is bringing ideas to life or leading
+          collaborative efforts.
+        </p>
+      </div>
+      <img
+        className="w-full my-5"
+        src="http://ghchart.rshah.org/sijirama"
+        alt="sijirama's Github chart"
+      />
+      <div className="w-full my-4">
+        <p className="font-extrabold -tracking-wide text-lg">Work Experience</p>
+        {expereince.map((exp, i) => (
+          <div className="my-4 md:my-3" key={i}>
+            <div className="flex items-center justify-between">
+              <div className="flex-col-reverse justify-start flex items-start md:flex-row md:items-center gap-2 md:gap-3 my-2 md:my-1 ">
+                <p className="font-semibold text-sm  md:text-base tracking-[0.010em] text-clip ">
+                  {exp.name}
+                </p>
+                <div className="text-xs bg-zinc-900 dark:bg-zinc-200 text-zinc-100 dark:text-black rounded-md py-0.5 px-1 md:px-3 font-semibold tracking-wide">
+                  {exp.mode}
+                </div>
+              </div>
+              <p className="text-xs md:text-sm tracking-wide">{exp.period}</p>
+            </div>
+            <p className="text-xs md:text-base font-light tracking-wide dark:text-zinc-300 text-zinc-900">
+              {exp.description}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="w-full">
+        <p className="font-extrabold -tracking-wide text-lg">Education</p>
+        {education.map((exp, i) => (
+          <div className="my-3" key={i}>
+            <div className="flex items-center justify-between">
+              <p className="font-semibold tracking-[0.010em] ">{exp.name}</p>
+              <p className="text-xs md:text-sm tracking-wide">{exp.period}</p>
+            </div>
+            <p className="text-xs md:text-sm font-light tracking-wide text-zinc-900 dark:text-zinc-300">
+              {exp.description}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="w-full">
+        <p className="font-extrabold -tracking-wide text-lg">Skills</p>
+        <div className="flex items-center flex-wrap gap-3 my-2">
+          {skills.map((skill, i) => (
+            <div
+              key={i}
+              className="text-xs bg-zinc-950 text-zinc-200 dark:bg-zinc-200 dark:text-black rounded-md py-0.5 px-2 md:px-3 font-semibold tracking-wide"
+            >
+              {skill}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full">
+        <p className="font-extrabold -tracking-wide text-lg mb-6">
+          Current Projects
+        </p>
+        <div className="flex flex-col gap-2">
+          {projects.map((project, i) => (
+            <a
+              href={project?.link}
+              key={i}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="p-3.5 hover:bg-zinc-200 dark:hover:bg-zinc-950 transition-colors duration-200 border border-zinc-400 dark:border-zinc-800 rounded-xl flex flex-col gap-4">
+                <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-2">
+                  <div className="space-y-1">
+                    <p className="text-sm md:text-base font-bold tracking-[0.010em]">
+                      {project.title}
                     </p>
-                    <p className="text-xs md:text-sm tracking-wide">
-                        Full Stack Engineer focused on building products with extra attention to details
+                    <p className="text-xs md:text-sm tracking-wide text-zinc-900 dark:text-zinc-100">
+                      {project.description}
                     </p>
-                    <p className="font-light text-xs tracking-wide flex gap-1 items-center justify-start">
-                        <SlGlobe className="" />
-                        Victoria Island, Lagos, NG.
-                    </p>
-                    <div className="w-full flex gap-2 flex-wrap py-1 md:py-3">
-                        {socials.map((social, i) => (
-                            <a key={i} href={`${social.url}`} target="_blank">
-                                <div className=" p-2 md:p-3 rounded-lg border border-zinc-500 dark:hover:bg-zinc-800 hover:bg-zinc-300 transition-colors duration-300">
-                                    {social.icon}
-                                </div>
-                            </a>
-                        ))}
+                  </div>
+                  <p className="text-xs md:text-sm text-zinc-600 dark:text-zinc-400 italic sm:text-right">
+                    {project.type}
+                  </p>
+                </div>
+                <div className="flex gap-2 flex-wrap py-1">
+                  {project.technologies.map((skill, i) => (
+                    <div
+                      key={i}
+                      className="text-xs bg-zinc-950 text-zinc-200 dark:bg-zinc-200 dark:text-black rounded-md py-0.5 px-2 md:px-3 font-semibold tracking-wide"
+                    >
+                      {skill}
                     </div>
+                  ))}
                 </div>
-                {/**/}
-                {/*
-                */}
-                <div
-                    style={{
-                        backgroundImage: `url(${lagos.src})`,
-                    }}
-                    className="w-1/5 hidden md:block rounded-lg bg-cover bg-center bg-no-repeat h-36 p-2 bg-red-800"
-                >
-                </div>
-
-            </div>
-            <div className="text-start  w-full flex flex-col gap-2">
-                <p className="font-extrabold -tracking-wide text-lg">About</p>
-                <p className="text-xs md:text-base tracking-wide text-zinc-900 dark:text-zinc-100">As a dedicated Full Stack Engineer, my passion lies in building technology that positively impacts and aids people. I am consistently excited about the opportunity to create, innovate, and contribute to meaningful solutions. Whether it is bringing ideas to life or leading collaborative efforts, I thrive on the constant journey of building and making a difference.</p>
-            </div>
-            <img className="w-full my-5" src="http://ghchart.rshah.org/sijirama" alt="sijirama's Github chart" />
-            <div className="w-full my-4">
-                <p className="font-extrabold -tracking-wide text-lg">Work Experience</p>
-                {expereince.map((exp, i) => (
-                    <div className="my-4 md:my-3" key={i}>
-                        <div className="flex items-center justify-between">
-                            <div className="flex-col-reverse justify-start flex items-start md:flex-row md:items-center gap-2 md:gap-3 my-2 md:my-1 ">
-                                <p className="font-semibold text-sm  md:text-base tracking-[0.010em] text-clip ">{exp.name}</p>
-                                <div className="text-xs bg-zinc-900 dark:bg-zinc-200 text-zinc-100 dark:text-black rounded-md py-0.5 px-1 md:px-3 font-semibold tracking-wide" >{exp.mode}</div>
-                            </div>
-                            <p className="text-xs md:text-sm tracking-wide">{exp.period}</p>
-                        </div>
-                        <p className="text-xs md:text-base font-light tracking-wide dark:text-zinc-300 text-zinc-900">
-                            {exp.description}
-                        </p>
-                    </div>
-                ))}
-            </div>
-            <div className="w-full">
-                <p className="font-extrabold -tracking-wide text-lg">Education</p>
-                {education.map((exp, i) => (
-                    <div className="my-3" key={i}>
-                        <div className="flex items-center justify-between">
-                            <p className="font-semibold tracking-[0.010em] ">{exp.name}</p>
-                            <p className="text-xs md:text-sm tracking-wide">{exp.period}</p>
-                        </div>
-                        <p className="text-xs md:text-sm font-light tracking-wide text-zinc-900 dark:text-zinc-300">
-                            {exp.description}
-                        </p>
-                    </div>
-                ))}
-            </div>
-            <div className="w-full">
-                <p className="font-extrabold -tracking-wide text-lg">Skills</p>
-                <div className="flex items-center flex-wrap gap-3 my-2">
-                    {skills.map((skill, i) => (
-                        <div key={i} className="text-xs bg-zinc-950 text-zinc-200 dark:bg-zinc-200 dark:text-black rounded-md py-0.5 px-2 md:px-3 font-semibold tracking-wide" >{skill}</div>
-                    ))}
-                </div>
-            </div>
-            <div className="w-full">
-                <p className="font-extrabold -tracking-wide text-lg">Projects</p>
-                <div className="items-center gap-3 my-2 grid grid-cols-2 md:grid-cols-3">
-                    {projects.map((project, i) => (
-                        <a href={project?.link} key={i} target="_blank">
-                            <div className="p-2 md:p-3 h-fit hover:bg-zinc-200 dark:hover:bg-zinc-950 transition-colors duration-200 border border-zinc-400 dark:border-zinc-800 rounded-xl flex flex-col justify-between gap-7">
-                                <div className="space-y-2">
-                                    <p className="text-sm md:text-base font-bold tracking-[0.010em]">{project.title}</p>
-                                    <p className="text-xs md:text-sm tracking-wide text-zinc-900 dark:text-zinc-100">{project.description}</p>
-                                </div>
-                                <div className="flex gap-2 flex-wrap py-1">
-                                    {project.technologies.map((skill, i) => (
-                                        <div key={i} className="text-xs bg-zinc-950 text-zinc-200 dark:bg-zinc-200 dark:text-black rounded-md py-0.5 px-2 md:px-3 font-semibold tracking-wide" >{skill}</div>
-                                    ))}
-                                </div>
-                            </div>
-                        </a>
-                    ))}
-                </div>
-            </div>
-        </main>
-    )
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -1,147 +1,114 @@
-
 export const expereince = [
-    {
-        role: "Backend Web Developer",
-        name: "Tradit Markets",
-        period: "Aug 2023 - October 2023",
-        mode: "Remote",
-        description: "Developed and implemented a backend server using NestJS for a service advertisement application. Designed and optimized functionalities to enhance the user experience and facilitate efficient service promotion. Leveraged NestJS robust capabilities to create a seamless and responsive backend infrastructure for the application."
-    },
-    {
-        role: "Full Stack Web Developer Intern",
-        name: "ChamsAccess Nigeria",
-        period: "Jan 2023 - July 2023",
-        mode: "Hybrid",
-        description: "Revamped the Chamsaccess BOS web platform to optimize user experience and streamline complaint management processes. Developed the frontend using a modern and efficient technology stack, including Typescript, React, and C# (.Net), leading to improved performance. Collaborated with a team of developers to iteratively enhance project functionalities, contributing to a significant boost in customer experience scores."
-    },
-    {
-        role: "Full Stack Web Developer Intern",
-        name: "Sunu Assurances Nigeria",
-        period: "May 2022 - August 2022",
-        mode: "Onsite",
-        description: "Developed a Django Web app exclusively for company use, streamlining the online handling of technical complaints and resources. Collaborated with developers to conduct user research and iterated on design for optimal functionality, resulting in a substantial increase in user satisfaction. Implemented the web application, achieving a more efficient working environment for staff."
-    }
-]
+  {
+    role: "Fullstack Software developer",
+    name: "Nject",
+    period: "September 2024 - Present",
+    mode: "Remote",
+    description:
+      "Developed engaging and accessible frontend features using Next.js, enhancing user experience and platform usability. Played a key role in migrating the backend from Python to Golang, leading to a 5x increase in speed and performance. Optimized system scalability and response times, ensuring high reliability and seamless support for a growing user base.",
+  },
+  {
+    role: "Backend Web Developer",
+    name: "Tradit Markets",
+    period: "Aug 2023 - October 2023",
+    mode: "Remote",
+    description:
+      "Completely implemented a backend server using NestJS for a service advertisement application. Designed and optimized functionalities to enhance the user experience and facilitate efficient service promotion. Leveraged NestJS robust capabilities to create a seamless and responsive backend infrastructure for the application.",
+  },
+  {
+    role: "Full Stack Web Developer Intern",
+    name: "ChamsAccess Nigeria",
+    period: "Jan 2023 - July 2023",
+    mode: "Hybrid",
+    description:
+      "Revamped the Chamsaccess BOS web platform to optimize user experience and streamline complaint management processes. Developed the frontend using a modern and efficient technology stack, including Typescript, React, and C# (.Net), leading to improved performance. Collaborated with a team of developers to iteratively enhance project functionalities, contributing to a significant boost in customer experience scores.",
+  },
+  {
+    role: "Full Stack Web Developer Intern",
+    name: "Sunu Assurances Nigeria",
+    period: "May 2022 - August 2022",
+    mode: "Onsite",
+    description:
+      "Developed a Django Web app exclusively for company use, streamlining the online handling of technical complaints and resources. Collaborated with developers to conduct user research and iterated on design for optimal functionality, resulting in a substantial increase in user satisfaction. Implemented the web application, achieving a more efficient working environment for staff.",
+  },
+];
 
 export const education = [
-    {
-        name: "Babcock Unversity",
-        description: "Bachelor's Degree in Software Engineering",
-        period: "Sept 2020 - June 2024!!!"
-    }
-]
+  {
+    name: "Babcock Unversity",
+    description: "Bachelor's Degree in Software Engineering",
+    period: "Sept 2020 - June 2024!!!",
+  },
+];
 
 export const skills = [
-    "TypeScript/JavaScript",
-    "Golang",
-    "Rust",
-    "C/C++",
-    "NextJS/React",
-    "Node.Js",
-    "TailwindCSS",
-    "Docker",
-    "PostgreSQL",
-    "MongoDB",
-    "MySQL/MariaDB",
-    "Redis",
-    "Git/Github",
-    "Neovim/Vim",
-    "Linux (ARCH BTW)",
-    "Kafka",
-    "Sysadmin, i can and do self host and manage my servers",
-    "Devops"
-]
+  "TypeScript/JavaScript",
+  "Golang",
+  "Zig",
+  "C/C++",
+  "NextJS/React",
+  "Node.Js",
+  "TailwindCSS",
+  "Docker",
+  "PostgreSQL",
+  "MongoDB",
+  "MySQL/MariaDB",
+  "Redis",
+  "Git/Github",
+  "Neovim/Vim",
+  "Linux (ARCH BTW)",
+  "Kafka",
+  "Sysadmin, i can and do self host and manage my servers",
+  "Devops",
+];
 
 export const projects = [
-    {
-        "title": "BuymeZobo",
-        "description": "with buymezobo we are trying to create an easy way for nigerian creators or even anyone with an audience to monetize their audience and use that moola to pursue other endeavours and projects, we want to make creators independent",
-        "type": "Main Project",
-        "link": "https://buymezobo.vercel.app/",
-        "technologies": ["TypeScript", "Next.js", "TailwindCSS", "PostgreSQL", "Prisma"]
-    },
-    {
-        "title": "Sentinel",
-        "description": "a present work in progress, sentinel is a simple status page for my services and projects",
-        "type": "Side Project",
-        "link": "",
-        "technologies": ["Golang", "HTMX", "TailwindCSS", "Sqlite"]
-    },
-    {
-        "title": "Soro",
-        "description": "this is a serious work in progress and should not even be here, i'm currently on freedays doing research to build a programming language or rather the interpreter for my language, called soro, the challenge here is that i want to use boken english (pidgin) for the keywords, i just finished the tokenizer with flex and i'm having fun with it",
-        "type": "Research",
-        "link": "https://github.com/sijirama/interpreter-implementation-research",
-        "technologies": ["C++", "Golang"]
-    },
-    {
-        "title": "Seapick",
-        "description": "iphone bros should be angry but i wanted to clone airdrop, so this is is a simple file transfer over TCP, it works okay rn (i think), but i want to add bluetooth discovery to that, and maybe a web interface",
-        "type": "Side Project",
-        "technologies": ["Golang"]
-    },
-    {
-        "title": "Bauble",
-        "description": "Streamlining university project management for student groups and ensures an efficient project experience.",
-        "type": "Final Year Project",
-        "link": "https://baublespace.vercel.app/",
-        "technologies": ["TypeScript", "Next.js", "TailwindCSS", "PostgreSQL", "Prisma"]
-    },
-    {
-        "title": "Monko (WIP)",
-        "description": "simple experimental interpreter built in Golang, mostly for research on lexical analysis and token parsing",
-        "type": "Side Project",
-        "link": "",
-        "technologies": ["Golang"]
-    },
-    {
-        "title": "Alma",
-        "description": "Online community hub for university students",
-        "type": "Side Project",
-        "technologies": ["NextJS", "TailwindCSS", "PostgreSQL", "Redis", "Docker"]
-    },
-    {
-        "title": "Accord",
-        "description": "fullstack messaging application inspired by Discord, channel management, real-time messaging etc.",
-        "type": "Final Year Project",
-        "link": "https://accordingly.vercel.app/",
-        "technologies": ["TypeScript", "Next.js", "TailwindCSS", "PostgreSQL", "Prisma"]
-    },
+  {
+    title: "Soro",
+    description:
+      "i'm currently on freedays doing research to build a programming language or rather the interpreter for my language, called soro, the challenge here is that i want to use boken english (pidgin) for the keywords, i just finished the virtual machine and have been adding parts during my free time, check it out",
+    type: "Research",
+    link: "https://github.com/sijirama/soro",
+    technologies: ["Zig"],
+  },
+  {
+    title: "Turva",
+    description:
+      "real-time crowd-sourced danger alert platform for small communities, designed to empower residents with instant updates on local risks like crime, natural hazards, or emergencies. Built with a focus on simplicity and reliability, it allows users to report incidents, share geolocated alerts, and receive notifications, fostering safer and more connected neighborhoods.",
+    type: "Project",
+    link: "https://turva.co",
+    technologies: ["Golang", "React"],
+  },
 
-    {
-        "title": "oluwasijibomi.me",
-        "description": "My personal website and blog. Built with Next.js ",
-        "type": "Side Project",
-        "technologies": ["Next.js", "MDX", "TailwindCSS"]
-    },
-    {
-        "title": "Tidy",
-        "description": "CLI application for organization and task tracking",
-        "type": "Side Project",
-        "link": "https://github.com/sijirama/tidy",
-        "technologies": ["GoLang", "SQlite", "BubbleTea"]
-    },
-    {
-        "title": "LRU",
-        "description": "LRU cache implemented in golang",
-        "type": "Side Project",
-        "technologies": ["Golang"]
-    },
-
-    {
-        "title": "Chip*",
-        "description": "Simple chip8 emulator built with javascript.",
-        "type": "Side Project",
-        "link": "https://sijirama.github.io/chip8/",
-        "technologies": ["JavaScript"]
-    },
-    {
-        "title": "Particles",
-        "description": "research repository for particle life cycles under various conditions.",
-        "type": "Side Project",
-        "link": "https://sijirama.github.io/particles/",
-        "technologies": ["JavaScript"]
-    },
-
-
-]
+  /*
+  {
+    title: "BuymeZobo",
+    description:
+      "with buymezobo we are trying to create an easy way for nigerian creators or even anyone with an audience to monetize their audience and use that moola to pursue other endeavours and projects, we want to make creators independent",
+    type: "Main Project",
+    link: "https://buymezobo.ng/",
+    technologies: [
+      "TypeScript",
+      "Next.js",
+      "TailwindCSS",
+      "PostgreSQL",
+      "Prisma",
+    ],
+  },
+  {
+    title: "Seapick",
+    description:
+      "iphone bros should be angry but i wanted to clone airdrop, so this is is a simple file transfer over TCP, it works okay rn (i think), but i want to add bluetooth discovery to that, and maybe a web interface",
+    type: "Side Project",
+    technologies: ["Golang"],
+  },
+  {
+    title: "Monkey",
+    description:
+      "simple experimental interpreter built in Golang, mostly for research on lexical analysis and token parsing",
+    type: "Side Project",
+    link: "",
+    technologies: ["Golang"],
+  },
+  */
+];
